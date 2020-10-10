efe73b3f0a694710da224d4f8156375baccc1987
zhuangruitao <1335877357@qq.com>
2020-10-10 17:18:47 +0800

    模拟newFixedThreadPool的OOM情况

---
+++ src/main/java/com/zrt/juc/FixedThreadPoolOOM.java
@@ -0,0 +1,30 @@
+package com.zrt.juc;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 模拟newFixedThreadPool的OOM情况
+ */
+
+public class FixedThreadPoolOOM {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        for(int i=0;i<Integer.MAX_VALUE;i++){
+            executorService.execute(new Task());
+        }
+    }
+}
+class Task implements  Runnable{
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000000000);
+            System.out.println(Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
